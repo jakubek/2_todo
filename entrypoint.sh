1a8c24ce09640c3286d0d427be33391695cca9a5
Jakub Mroziński <jakub@31337.pl>
2022-04-10 19:10:20 +0200

    add entrypoint.s

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+set -e
+for s in $( jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" /mnt/secrets/*); do
+    export $s
+done
+
+export DB_USER=${username}
+export DB_PASS=${password}
+export DB_HOST=${host}
+export DB_PORT=${port}
+export DB_NAME=todo
+
+python manage.py migrate 
+python manage.py runserver 0.0.0.0:8000
