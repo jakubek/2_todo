4b7b723050cc11ab81f2f4157651f2d48afa42ce
Jakub Mroziński <jakub@31337.pl>
2022-04-10 19:49:50 +0200

    change DB_NAMe

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export DB_USER=${username}
 export DB_PASS=${password}
 export DB_HOST=$(echo ${host} | awk -F: '{print $1}')
 export DB_PORT=${port}
-export DB_NAME=todo
+export DB_NAME=mydb
 
 python manage.py migrate 
 python manage.py runserver 0.0.0.0:8000
